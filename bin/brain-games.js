de8a14e487a4f654029fd61cb0e6153922f17513
Nikolai <mor97nik@mail.ru>
2023-05-14 18:58:29 +0300

    add src, add sayGreetings

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+import sayGreetings from "../src/cli";
 
-console.log("Welcome to the Brain Games!")
+sayGreetings();
---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import redlineSync from 'readline-sync';
+
+const sayGreetings = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = redlineSync.question('May I have your name?');
+    console.log('Hello, ${name}!');
+};
+
+export default sayGreetings;
