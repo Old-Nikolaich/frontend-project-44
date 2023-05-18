693b933767f189b456531b5471a4295f267a019d
Nikolai <mor97nik@mail.ru>
2023-05-19 00:10:45 +0300

    npm work, second step work, fix make brain-games

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import redlineSync from 'readline-sync';
 const sayGreetings = () => {
     console.log('Welcome to the Brain Games!');
     const name = redlineSync.question('May I have your name?');
-    console.log('Hello, ${name}!');
+    console.log('Hello, ' + name + '!');
 };
 
 export default sayGreetings;
