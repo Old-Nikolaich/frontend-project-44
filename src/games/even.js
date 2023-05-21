7cc041ce46d6498f722cbcf5d51b6a1f3b466746
Nikolai <mor97nik@mail.ru>
2023-05-21 18:06:20 +0300

    Pass fifth step, add index.js, add getRandomInt

---
+++ src/games/even.js
@@ -0,0 +1,25 @@
+import rulesOfGames from '../index.js';
+import getRandomInt from '../getRandomInt.js';
+
+// Определение функции игры на чётность
+const evenGame = () => {
+  // Задание игры
+  const noteToEven = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+  // Вводные данные на игру
+  const taskEven = () => {
+    const number = getRandomInt(100);
+
+    // Определяем функцию для проверки на четность числа
+    const isEven = (num) => num % 2 === 0;
+
+    const question = `${number}`;
+    const result = (isEven(number) ? 'yes' : 'no');
+
+    return [question, result];
+  };
+
+  rulesOfGames(noteToEven, taskEven);
+};
+
+export default evenGame;
